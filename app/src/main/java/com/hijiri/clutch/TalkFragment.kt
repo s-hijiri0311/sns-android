99ab80a0f3114f54c722d05ccc2d821ad158de64
s_hijiri0311 <suzuki.hijiri@itolab.nitech.ac.jp>
2020-07-03 16:26:58 +0900

    initialize

---
+++ app/src/main/java/com/hijiri/clutch/TalkFragment.kt
@@ -0,0 +1,23 @@
+package com.hijiri.clutch
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+
+class TalkFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_talk, container, false)
+        return view
+    }
+}
